71cabc4d9d9e02b576508c9665b67e1814da05c9
mannj2015 <72755084+mannj2015@users.noreply.github.com>
2020-10-19 17:45:22 -0400

    message

---
+++ rev_ex1/helloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿//dotnet new console -o helloWorld
+//1.0 prefefined namespace
+using System;
+
+//1.1 custom defined namespace
+namespace helloWorld
+{
+    class Program
+    {
+        //start execution
+            //commandline arguments
+        static void Main(string[] args)
+        {
+            string fname, lname;
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine("please enter fname");
+            fname = Console.ReadLine();
+            Console.WriteLine("please enter lname");
+            lname = Console.ReadLine();
+            //interpolation
+            Console.WriteLine($"welcome {fname} {lname}");
+            //connocation - less efficient
+            //System.Console.WriteLine("w " + fname + " " + lname);
+        }
+    }
+}
